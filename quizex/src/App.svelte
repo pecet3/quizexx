79717a29ade3b8923679e3e2c1128b13c70e440f
pecet3 <pecet3107@gmail.com>
2024-02-02 19:56:53 +0100

    test

--- quizex/src/App.svelte
+++ quizex/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
@@ -13,19 +13,21 @@
       <img src={svelteLogo} class="logo svelte p-10" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1 class="">Vite + Svelte</h1>
 
-  <div class="card p-8">
+  <div class="card bg-slate-500">
     <Counter />
   </div>
 
   <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">SvelteKit</a
+    >, the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
