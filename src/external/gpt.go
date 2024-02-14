b7abe459e3fd0c027ce3f085a3e07b64376af7e3
pecet3 <pecet3107@gmail.com>
2024-02-15 00:40:04 +0100

    fetch gpt

---
+++ src/external/gpt.go
@@ -0,0 +1,55 @@
+package external
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/joho/godotenv"
+)
+
+const (
+	apiEndpoint = "https://api.openai.com/v1/chat/completions"
+)
+
+func FetchGPT() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		fmt.Println("error env")
+	}
+
+	apiKey := os.Getenv("KEY")
+
+	client := resty.New()
+
+	prompt := "hej, odpowiedz"
+	userInput := ""
+
+	response, err := client.R().
+		SetAuthToken(apiKey).
+		SetHeader("Content-Type", "application/json").
+		SetBody(map[string]interface{}{
+			"model":      "gpt-3.5-turbo",
+			"messages":   []interface{}{map[string]interface{}{"role": "system", "content": prompt + userInput}},
+			"max_tokens": 50,
+		}).
+		Post(apiEndpoint)
+
+	if err != nil {
+		fmt.Println("connecting with api error")
+	}
+
+	body := response.Body()
+	var data map[string]interface{}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error while decoding JSON response:", err)
+		return
+	}
+
+	content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+	fmt.Println(content)
+}
