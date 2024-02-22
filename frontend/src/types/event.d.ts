799e36cdf27ba598ebf7dcc4dc883d8383857d8d
pecet3 <pecet3107@gmail.com>
2024-02-22 19:56:04 +0100

    initial, added types

---
+++ frontend/src/types/event.d.ts
@@ -0,0 +1,52 @@
+interface IEvent {
+    type: "ready_status" | "room_settings" | "update_gamestate" | "server_message" | "finish_game";
+    payload: any;
+};
+
+type TReadyStatus = {
+    clients: ReadyClient[];
+};
+type TReadyClient = {
+    name: string;
+    isReady: boolean;
+};
+type TSettings = {
+    name: string;
+    category: string;
+    difficulty: string;
+    maxRounds: string;
+};
+
+type TServerMessage = {
+    message: string;
+};
+
+type TGameState = {
+    round: number;
+    question: string;
+    answers: string[];
+    actions: RoundAction[];
+    score: PlayerScore[];
+    playersFinished: string[];
+};
+type TRoundAction = {
+    name: string;
+    answer: number;
+    round: number;
+};
+type TPlayerScore = {
+    name: string;
+    points: number;
+    roundsWon: number[];
+};
+
+
+type TFinishGame = bool
+
+
+// in the future we will use it
+// type TSendMessageEvent = {
+//     userName: string;
+//     message: string;
+// };
+/////// 
