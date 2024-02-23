b834d490bdc17ad46001538d3a027a4b7200df82
Grzegorz Pacewicz <86358105+GrzegorzPacewicz@users.noreply.github.com>
2024-02-23 14:34:14 +0100

    Room added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { CreateRoom } from "./components/CreateRoom/CreateRoom";
 import { MainView } from "./components/MainView/MainView";
+import { Room } from "./components/Room/Room";
 
 function App() {
   return (
     <>
       <MainView />
       <CreateRoom />
+      <Room />
     </>
   );
 }
