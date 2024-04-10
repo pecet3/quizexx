adc67511989d15b8946daa72de8a2f5543a7c6d5
pecet3 <pecet3107@gmail.com>
2024-04-10 21:32:51 +0200

    working fetching using context and standard lib

--- src/external/gpt.go
+++ src/external/gpt.go
@@ -1,8 +1,17 @@
 package external
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
 const (
@@ -26,51 +35,61 @@ func (e ExternalService) SaveQuestionSetToDB(db *sql.DB) {
 
 }
 func (e ExternalService) FetchQuestionSet(ctx context.Context, category, maxRounds, difficulty, lang string) (string, error) {
-	// log.Println(ctx)
-	// err := godotenv.Load(".env")
+	log.Println(ctx)
+	err := godotenv.Load(".env")
 
-	// if err != nil {
-	// 	fmt.Println("error env")
-	// }
+	if err != nil {
+		fmt.Println("error env")
+	}
 
-	// apiKey := os.Getenv("GPT_KEY")
+	apiKey := os.Getenv("GPT_KEY")
 
-	// client := resty.New()
-	// options := "Options for this quiz:" +
-	// 	" category: " + category +
-	// 	", diffuculty:" + difficulty +
-	// 	", content language: " + lang
+	options := "Options for this quiz:" +
+		" category: " + category +
+		", diffuculty:" + difficulty +
+		", content language: " + lang
 
-	// prompt := "return json for quiz game with " +
-	// 	maxRounds + " questions." +
-	// 	options +
-	// 	" You have to return correct struct. This is just array of objects. Nothing more, start struct: [{ question, 4x answers, correctAnswer(index)}] "
+	prompt := "return json for quiz game with " +
+		maxRounds + " questions." +
+		options +
+		" You have to return correct struct. This is just array of objects. Nothing more, start struct: [{ question, 4x answers, correctAnswer(index)}] "
 
-	// response, err := client.R().
-	// 	SetAuthToken(apiKey).
-	// 	SetHeader("Content-Type", "application/json").
-	// 	SetBody(map[string]interface{}{
-	// 		"model":      "gpt-3.5-turbo",
-	// 		"messages":   []interface{}{map[string]interface{}{"role": "system", "content": prompt}},
-	// 		"max_tokens": 1200,
-	// 	}).
-	// 	Post(apiEndpoint)
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
 
-	// if err != nil {
-	// 	fmt.Println("connecting with api error")
-	// 	return "", err
-	// }
+	reqBody, err := json.Marshal(map[string]interface{}{
+		"model":      "gpt-3.5-turbo",
+		"messages":   []interface{}{map[string]interface{}{"role": "system", "content": prompt}},
+		"max_tokens": 1200,
+	})
+	if err != nil {
+		return "", err
+	}
 
-	// body := response.Body()
-	// var data map[string]interface{}
+	req, err := http.NewRequestWithContext(ctx, "POST", apiEndpoint, bytes.NewBuffer(reqBody))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiKey)
 
-	// err = json.Unmarshal(body, &data)
-	// if err != nil {
-	// 	return "", err
-	// }
-	// content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
 
-	content := `[ { "question": "Która marka samochodu pochodzi z Włoch?", "answers": ["Volkswagen", "Toyota", "Fiat", "Ford"], "correctAnswer": 2 }, { "question": "Jak nazywa się popularny model auta marki Mercedes-Benz?", "answers": ["Astra", "Passat", "Clio", "Klasa E"], "correctAnswer": 3 }, { "question": "Co oznacza skrót 'SUV' w motoryzacji?", "answers": ["Super Ultra Vitesse", "Sport Utility Vehicle", "Special Upgrade Version", "Society of United Vehicles"], "correctAnswer": 1 }, { "question": "Która marka samochodu pochodzi z Japonii?", "answers": ["BMW", "Honda", "Audi", "Chevrolet"], "correctAnswer": 1 } ]`
+	var data map[string]interface{}
+
+	err = json.NewDecoder(response.Body).Decode(&data)
+	if err != nil {
+		return "", err
+	}
+
+	content := data["choices"].([]interface{})[0].(map[string]interface{})["message"].(map[string]interface{})["content"].(string)
+
+	// content := `[ { "question": "Która marka samochodu pochodzi z Włoch?", "answers": ["Volkswagen", "Toyota", "Fiat", "Ford"], "correctAnswer": 2 }, { "question": "Jak nazywa się popularny model auta marki Mercedes-Benz?", "answers": ["Astra", "Passat", "Clio", "Klasa E"], "correctAnswer": 3 }, { "question": "Co oznacza skrót 'SUV' w motoryzacji?", "answers": ["Super Ultra Vitesse", "Sport Utility Vehicle", "Special Upgrade Version", "Society of United Vehicles"], "correctAnswer": 1 }, { "question": "Która marka samochodu pochodzi z Japonii?", "answers": ["BMW", "Honda", "Audi", "Chevrolet"], "correctAnswer": 1 } ]`
 
 	return content, nil
 }
