6f978df117368f2dee887c9167dd119f94302de8
pecet3 <pecet3107@gmail.com>
2024-04-15 20:46:09 +0200

    .

--- src/main.go
+++ src/main.go
@@ -6,8 +6,10 @@ import (
 	"github.com/pecet3/quizex/app"
 )
 
-// I know I wrote shitty code but it works...
-// Maybe I'd refactore it in the future but now I wanna be focused on the next thinks
+// I know I wrote shitty code but it works... and I am aware it sucks
+// I've tried understand how interfaces work and I know now that is no need to implement them here.
+// Maybe I'd refactore it in the future but now I wanna be focused on the next projects
+
 func main() {
 	log.Println("Running the server...")
 	app.Run()
