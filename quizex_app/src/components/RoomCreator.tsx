b58f411623aca3bffadf572bc1e3f7032960e4c9
Jakub <pecet3107@gmail.com>
2025-01-23 21:51:24 +0100

    second for answer implemented

--- quizex_app/src/components/RoomCreator.tsx
+++ quizex_app/src/components/RoomCreator.tsx
@@ -31,27 +31,6 @@ export const RoomCreator: React.FC<{
         <button id="randomCategory" className="text-sm" type="button">
           [Get random category]
         </button>
-        <label className="rounded-lg text-lg font-bold underline">
-          Difficulty Level:
-        </label>
-        <select
-          id="difficulty"
-          name="difficulty"
-          className="p-0.5 text-2xl rounded-sm font m-auto border border-black bg-white placeholder:text-gray-400 placeholder:text-center text-black text-center"
-        >
-          <option value="easy" className="text-center">
-            Easy
-          </option>
-          <option value="medium" className="text-center">
-            Medium
-          </option>
-          <option value="hard" className="text-center">
-            Hard
-          </option>
-          <option value="veryhard" className="text-center">
-            Very Hard
-          </option>
-        </select>
         <div className="flex font-mono sm:flex-row flex-col gap-4">
           <div className="flex flex-col">
             <label className="rounded-lg font-mono text-xl font-bold underline">
@@ -63,13 +42,28 @@ export const RoomCreator: React.FC<{
               className="p-0.5 text-2xl rounded-sm font m-auto border border-black bg-white placeholder:text-gray-400 placeholder:text-center text-black text-center"
             >
               <option value="4" className="text-center">
-                4
+                3
               </option>
               <option value="5" className="text-center">
+                4
+              </option>
+              <option value="6" className="text-center">
                 5
               </option>
               <option value="6" className="text-center">
                 6
+              </option>
+              <option value="6" className="text-center">
+                7
+              </option>
+              <option value="6" className="text-center">
+                8
+              </option>
+              <option value="6" className="text-center">
+                9
+              </option>
+              <option value="6" className="text-center">
+                10
               </option>
             </select>
           </div>
@@ -87,6 +81,60 @@ export const RoomCreator: React.FC<{
               </option>
               <option value="english" className="text-center">
                 English
+              </option>
+            </select>
+          </div>
+        </div>
+        <div className="flex font-mono sm:flex-row flex-col gap-4 items-end">
+          <div className="flex flex-col">
+            <label className="rounded-lg text-lg font-bold underline">
+              Difficulty:
+            </label>
+            <select
+              id="difficulty"
+              name="difficulty"
+              className="p-0.5 text-2xl rounded-sm font m-auto border border-black bg-white placeholder:text-gray-400 placeholder:text-center text-black text-center"
+            >
+              <option value="easy" className="text-center">
+                Easy
+              </option>
+              <option value="medium" className="text-center">
+                Medium
+              </option>
+              <option value="hard" className="text-center">
+                Hard
+              </option>
+              <option value="veryhard" className="text-center">
+                Very Hard
+              </option>
+            </select>
+          </div>
+          <div className="flex flex-col">
+            <label className="rounded-lg text-sm font-bold underline w-24 break-words">
+              Seconds for answer:
+            </label>
+            <select
+              id="sec_for_answer"
+              name="sec_for_answer"
+              className="p-0.5 text-2xl rounded-sm font m-auto border border-black bg-white placeholder:text-gray-400 placeholder:text-center text-black text-center"
+            >
+              <option value="10" className="text-center">
+                10
+              </option>
+              <option value="15" className="text-center">
+                15
+              </option>
+              <option value="20" className="text-center">
+                20
+              </option>
+              <option value="30" className="text-center">
+                30
+              </option>
+              <option value="45" className="text-center">
+                45
+              </option>
+              <option value="60" className="text-center">
+                60
               </option>
             </select>
           </div>
