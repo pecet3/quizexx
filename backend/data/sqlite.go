e5e933a2c61cb2e56fc5495d5c1aec3edc623965
Jakub <pecet3107@gmail.com>
2025-02-01 01:31:24 +0100

    rm entities

--- backend/data/sqlite.go
+++ backend/data/sqlite.go
@@ -5,39 +5,9 @@ import (
 	"log"
 	"strings"
 
-	"github.com/pecet3/quizex/data/entities"
 	"github.com/pecet3/quizex/pkg/logger"
 )
 
-func NewSQLiteTest() *sql.DB {
-	db, err := sql.Open("sqlite3", "./store.db")
-	if err != nil {
-		log.Fatalf("Failed to connect database: %v", err)
-	}
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatalf("Failed to ping database: %v", err)
-	}
-
-	prepareList := [5]string{
-		entities.UsersTable,
-		entities.SessionsTable,
-		entities.GameContentTable,
-		entities.GameContentRoundTable,
-		entities.GameContentAnswerTable,
-	}
-
-	for _, table := range prepareList {
-		if err := prepare(db, table); err != nil {
-			log.Fatalf("Failed to prepare DB: %v", err)
-		}
-	}
-
-	logger.Info("Preparing DB has been finished")
-
-	return db
-}
 func NewSQLite() *sql.DB {
 	db, err := sql.Open("sqlite3", "./store.db")
 	if err != nil {
